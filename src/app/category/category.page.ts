675364c5737383f9f83cfef271a2e1ccd7806159
gowtham.hariram <gowtham.hariram@infosys.com>
2019-10-23 11:43:48 +0530

    loading added in category page

--- src/app/category/category.page.ts
+++ src/app/category/category.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { RestApiService } from '../rest-api.service';
+import { LoadingController } from '@ionic/angular';
+
 @Component({
   selector: 'app-category',
   templateUrl: './category.page.html',
@@ -18,40 +20,29 @@ export class CategoryPage implements OnInit {
   };
   constructor(
     private router: Router,
-    private api: RestApiService
+    private api: RestApiService,
+    public loadingController: LoadingController,
   ) { }
 
   ngOnInit() {
     this.selLanguage = 'english';
     this.fetchCategory();
-    /*this.category = [
-      {
-        'id': 1,
-        'slug':'Basic-Theory-Test',
-        'name': 'Car Driving - the Basic Theory Test (BTT)',
-        'image': 'http://www.basictheorytestsg.com/test_pictures/69c00b986ae281c9_144125.jpg',
-        'count': '436'
-      },
-      {
-        'id': 1,
-        'slug':'Final-Theory-Test',
-        'name': 'Car Driving - the Final Theory Test (FTT)',
-        'image': 'http://www.basictheorytestsg.com/test_pictures/2e3433f9d9d22acc_144110.jpg',
-        'count': '500'
-      },
-      {
-        'id': 1,
-        'slug':'motorcycle-Theory-Test',
-        'name': 'Motorcycle Riding - the Theory Test (RTT)',
-        'image': 'http://www.basictheorytestsg.com/test_pictures/25662c351c5db417_144043.jpg',
-        'count': '200'
-      }
+  }
 
-    ]; */
+  async presentLoadingWithOptions() {
+    const loading = await this.loadingController.create({
+      spinner: 'circles',
+      message: 'Please wait...',
+      translucent: true,
+      cssClass: 'custom-class custom-loading'
+    });
+    return await loading.present();
   }
 
   fetchCategory() {
+    this.presentLoadingWithOptions();
     this.api.getStaticData('api/category').subscribe(result => {
+      this.loadingController.dismiss();
       const response: any = result;
       if (response.body !== undefined) {
         const res = response.body;
@@ -60,7 +51,6 @@ export class CategoryPage implements OnInit {
             this.category = res.data;
           } else {
             this.category = [];
-            // this.formError = res[0].message;
           }
         }
       }
